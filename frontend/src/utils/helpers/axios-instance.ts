8223fbac8f54b0c489259c2481fe0415a58f7bdd
cncoding101 <cnsd202@gmail.com>
2024-07-09 17:21:42 +0200

    init

---
+++ frontend/src/utils/helpers/axios-instance.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+
+const client = axios.create({
+  baseURL: import.meta.env.API_URL,
+});
+
+const request = (options: AxiosRequestConfig) => {
+  const onSuccess = (response: AxiosResponse) => {
+    console.debug('Request succesful!', response);
+    return response.data;
+  };
+
+  const onError = (error: AxiosError) => {
+    console.error('Request failed:', error.config);
+
+    if (error.response) {
+      console.error('Status:', error.response.status);
+      console.error('Data:', error.response.data);
+      console.error('Headers:', error.response.headers);
+    } else console.error('Error message:', error.message);
+
+    return Promise.reject(error.response || error.message);
+  };
+
+  return client(options).then(onSuccess).catch(onError);
+};
+
+export default request;
